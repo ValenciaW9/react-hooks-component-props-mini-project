f0d573d0f7182833053df0f3b3286f22e7fb6821
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-29 19:20:06 -0400

    react-hooks-component-props-mini-project/
    react-hooks-component-props-mini-project/

--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -1,36 +1,57 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import React from 'react';
 import ArticleList from "../components/ArticleList";
 
-import React from 'react';
-const posts = [
-  {
-    id: 1,
-    title: "Components 101",
-    date: "December 15, 2020",
-    preview: "Setting up the building blocks of your site",
-  },
-  {
-    id: 2,
-    title: "React Data Flow",
-    date: "December 11, 2020",
-    preview: "Passing props is never passé",
-  },
-  {
-    id: 3,
-    title: "Function Components vs Class Components",
-    date: "December 10, 2020",
-    preview: "React, meet OOJS.",
-  },
-];
-
 test("renders a <main> element", () => {
+  const posts = [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+    },
+    {
+      id: 3,
+      title: "Function Components vs Class Components",
+      date: "December 10, 2020",
+      preview: "React, meet OOJS.",
+    },
+  ];
+
   render(<ArticleList posts={posts} />);
   const mainElement = screen.getByRole("main");
   expect(mainElement).toBeInTheDocument();
 });
 
 test("renders an Article component for each post passed as a prop", () => {
+  const posts = [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+    },
+    {
+      id: 3,
+      title: "Function Components vs Class Components",
+      date: "December 10, 2020",
+      preview: "React, meet OOJS.",
+    },
+  ];
+
   render(<ArticleList posts={posts} />);
   const articleElements = screen.getAllByRole("article");
   expect(articleElements).toHaveLength(posts.length);
